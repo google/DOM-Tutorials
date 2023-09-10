e7fe3abc01c03bd2727afbb11a0a257bdcc0ca3d
Neil Fraser <fraser@google.com>
2023-09-10 08:47:47 +0200

    Remove 'cgi' dependency
    Python is removing cgi support from the next version.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-Copyright 2013-2020 Google LLC
+Copyright 2013-2023 Google LLC
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,29 +14,44 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import cgi
 from google.cloud import ndb
+from urllib.parse import unquote
 
 
 # Datastore model.
 class Grid(ndb.Model):
   data = ndb.StringProperty(required=True)
 
+# Parse a query string (e.g. a=1&b=2) into a dictionary (e.g. {"a": 1, "b": 2}).
+# Very minimal parser.  Does not combine repeated names (a=1&a=2), ignores
+# valueless names (a&b), does not support or isindex.
+def parse_query(query_string):
+  parts = query_string.split('&')
+  dict = {}
+  for part in parts:
+    tuple = part.split("=", 1)
+    if len(tuple) == 2:
+      dict[tuple[0]] = unquote(tuple[1])
+  return dict
 
 # Route to requested handler.
 def app(environ, start_response):
+  query_string = ""
+  if "QUERY_STRING" in environ:
+    query_string = environ["QUERY_STRING"]
+  query_dict = parse_query(query_string)
   if environ["PATH_INFO"] == "/":
-    return redirect(environ, start_response)
+    return redirect(query_dict, start_response)
   if environ["PATH_INFO"] == "/add.py":
-    return add(environ, start_response)
+    return add(query_dict, start_response)
   if environ["PATH_INFO"] == "/collab.py":
-    return collab(environ, start_response)
+    return collab(query_dict, start_response)
   start_response("404 Not Found", [])
   return [b"Page not found."]
 
 
 # Redirect for root directory.
-def redirect(environ, start_response):
+def redirect(query_dict, start_response):
   headers = [
     ("Location", "/static/index.html")
   ]
@@ -56,14 +71,13 @@ def num(s):
 
 
 # Add two numbers together.
-def add(environ, start_response):
-  forms = cgi.FieldStorage(environ=environ)
+def add(query_dict, start_response):
   a = 0
-  if "a" in forms:
-    a = num(forms["a"].value)
+  if "a" in query_dict:
+    a = num(query_dict["a"])
   b = 0
-  if "b" in forms:
-    b = num(forms["b"].value)
+  if "b" in query_dict:
+    b = num(query_dict["b"])
   out = str(a + b)
 
   headers = [
@@ -74,14 +88,13 @@ def add(environ, start_response):
 
 
 # Collaborative star grid.
-def collab(environ, start_response):
-  forms = cgi.FieldStorage(fp=environ['wsgi.input'], environ=environ)
+def collab(query_dict, start_response):
   try:
-    n = int(forms["n"].value)
+    n = int(query_dict["n"])
   except:
     n = -1
   try:
-    s = forms["s"].value
+    s = query_dict["s"]
   except:
     s = None
   isUpdate = (0 <= n <= 99) and (s in ("0", "1"))
